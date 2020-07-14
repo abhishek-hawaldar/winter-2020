18d426332ed9da8ea95946c5e1ec6aaa3e12e5d1
abhishek-hawaldar <54952930+abhishek-hawaldar@users.noreply.github.com>
2020-07-14 17:16:33 +0530

    Add files via upload

--- luckygogling.py
+++ luckygogling.py
@@ -1,28 +1,40 @@
 import bs4,requests,webbrowser
 from bs4 import BeautifulSoup
+import re
+
+
+#mo = phoneNumRegex.search('My number is 415-555-4242.')
+#print('Phone number found: ' + mo.group())
 
 
 
-
-a=input("ENTER THE TERM YOU WANT TO SEARCH:")
+phoneNumRegex = re.compile(r'\d\d\d\d\d\d \d\d\d\d\d')
+a="doctors near me phone number"
 b=a.split(' ')
-print('googling....')
+print('finding contact details for nearby doctors... ')
 res= requests.get('https://google.com/search?q=' + '+'.join(b))
+#webbrowser.open('https://google.com/search?q=' + '+'.join(b))
 res.raise_for_status()
-print(res.text)
+#print(res.text)
+
+mo = phoneNumRegex.findall(res.text)
+for i in range (len(mo)):
+
+    print('\nPhone number found: ' + mo[i])
 
 
-trl=open('htmlpage.html')
-trsoup=BeautifulSoup(trl.read(), "html.parser")
-trlinkelems=trsoup.select('.r a')
-print(trlinkelems[0])
 
-soup=BeautifulSoup(res.text, "html.parser")
+#trl=open('res.text')
+#trsoup=BeautifulSoup(trl.read(), "html.parser")
+#trlinkelems=trsoup.select('.r a')
+#print(trlinkelems[0])
 
-linkelems=soup.select('.r a')
-print("length is :",len(linkelems))
-num=min(5,len(linkelems))
+#soup=BeautifulSoup(res.text, "html.parser")
 
-for i in range(10):
-    print('opening result : ', linkelems[i].get('href'))
-    webbrowser.open('http://google.com'+linkelems[i].get('href'))
+#linkelems=soup.select('.r a')
+#print("length is :",len(linkelems))
+#num=min(5,len(linkelems))
+
+#for i in range(10):
+    #print('opening result : ', linkelems[i].get('href'))
+    #webbrowser.open('http://google.com'+linkelems[i].get('href'))
